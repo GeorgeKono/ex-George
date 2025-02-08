f1bb121de05e480d6a90037531617b93c2bb35ad
George Konovalov <george.konovalov@gmail.com>
2025-02-08 20:20:20 +0200

    Init repo

---
+++ pages/About.jsx
@@ -0,0 +1,8 @@
+export function About() {
+    return (
+        <section className="about">
+            <h1>About books and us...</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta rem error non culpa dolore, sed ut debitis quae et. Quae quo, ad debitis illum eius voluptatum hic rem fugiat dignissimos.</p>
+        </section>
+    )
+}
---
+++ pages/BookIndex.jsx
@@ -0,0 +1,7 @@
+export function BookIndex() {
+    return (
+        <section className="book-index">
+            <h1>Book index!</h1>
+        </section>
+    )
+}
