859e8a3432f33aef5f7034903589f8eedeb848e1
George Konovalov <george.konovalov@gmail.com>
2025-02-08 21:20:20 +0200

    Setup services and storage

--- pages/BookIndex.jsx
+++ pages/BookIndex.jsx
@@ -1,4 +1,22 @@
+import { bookService } from "../services/book.service.js"
+
+const { useState, useEffect, useRef } = React
+ 
 export function BookIndex() {
+    
+    const [books, setBooks] = useState(null)
+    const [filter, setFilter] = useState(bookService.getDefaultFilter())
+
+    useEffect(() => {
+        loadBooks()
+    }, [filter])
+
+    function loadBooks() {
+        bookService.query(filter).then(books => {
+            setBooks(books)
+        })
+    }
+    
     return (
         <section className="book-index">
             <h1>Book index!</h1>
---
+++ services/book.service.js
@@ -0,0 +1,80 @@
+import { storageService } from "./async-storage.service.js";
+import { utilService } from "./util.service.js";
+import { books as booksData} from "./books.js";
+
+export const bookService = {
+    query,
+    get,
+    remove,
+    save,
+    getEmptyBook,
+    getDefaultFilter,
+}
+
+const BOOK_KEY = 'bookDB'
+
+
+function query(filterBy = {}) {
+    return storageService.query(BOOK_KEY)
+        .then(books => {
+            if (!books || !books.length) {
+                books = booksData
+                utilService.saveToStorage(BOOK_KEY, books)
+            }
+            if (filterBy.title) {
+                const regExp = new RegExp(filterBy.title, 'i')
+                books = books.filter(book => regExp.test(book.title))
+            }
+            if (filterBy.minPrice) {
+                books = books.filter(book => book.listPrice.amount >= filterBy.minPrice)
+            }
+            
+            return books
+
+        })
+}
+
+function get(bookId) {
+    return storageService.get(BOOK_KEY, bookId)
+}
+
+function remove(bookId) {
+    // return Promise.reject('Oh No!')
+    return storageService.remove(BOOK_KEY, bookId)
+}
+
+function save(book) {
+    if (book.id) {
+        return storageService.put(BOOK_KEY, book)
+    } else {
+        return storageService.post(BOOK_KEY, book)
+    }
+}
+
+function getEmptyBook(title = '', price = '') {
+    return { title, price }
+}
+
+function getDefaultFilter() {
+    return { title: '', minPrice: '' }
+}
+
+function _createBooks() {
+    let books = utilService.loadFromStorage(BOOK_KEY)
+    
+    if (!books || !books.length) {
+        books = [
+            _createBook('Harry Potter 1', 300),
+            _createBook('Harry Potter 2', 120),
+            _createBook('Harry Potter 3', 50),
+            _createBook('Harry Potter 4', 150)
+        ]
+        utilService.saveToStorage(BOOK_KEY, books)
+    }
+}
+
+function _createBook(title, price = 250) {
+    const book = getEmptyBook(title, price)
+    book.id = utilService.makeId()
+    return book
+}
