b4c3c78d3fa2c1fdd990fa83d7d502cf2bb7df52
George Konovalov <george.konovalov@gmail.com>
2025-02-14 16:02:41 +0200

    Change AppHeader and add cursor for logo

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,13 +1,13 @@
-import { AppHeader } from "./cmps/AppHeader.jsx"
-import { About } from "./pages/About.jsx"
-import { Home } from "./pages/Home.jsx"
-import { BookIndex } from "./pages/BookIndex.jsx"
-
 const { useState } = React
 
-export function App() {
+import { AppHeader } from './cmps/AppHeader.jsx'
 
-    const [page, setPage] = useState('home')
+import { About } from './pages/About.jsx'
+import { Home } from './pages/Home.jsx'
+import { BookIndex } from './pages/BookIndex.jsx'
+
+export function App() {
+    const [page, setPage] = useState('book')
 
     function onSetPage(page) {
         setPage(page)
@@ -15,7 +15,9 @@ export function App() {
 
     return (
         <section className="app">
-            <AppHeader onSetPage={onSetPage}/>
+            <header>
+                <AppHeader onSetPage={onSetPage} />
+            </header>
 
             <main className="main-layout">
                 {page === 'home' && <Home onSetPage={onSetPage}/>}
--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -2,14 +2,15 @@ export function AppHeader({onSetPage}) {
     
     return (
         <header className="app-header full main-layout">
-            <React.Fragment>
-            <h1>Miss Books</h1>
+                <div className="logo" onClick={() => onSetPage('home')}>
+                    <h1>Miss Books</h1>
+                </div>
+
             <nav className="app-nav">
                 <a onClick={() => onSetPage('home')} href="#">Home</a>
                 <a onClick={() => onSetPage('about')} href="#">About</a>
-                <a onClick={() => onSetPage('book')}href="#">Books</a>
+                <a onClick={() => onSetPage('book')} href="#">Books</a>
             </nav>
-            </React.Fragment>
         </header>
     )
 }
